93236918125ef4f5c185d65574058d013df6ce00
floordiv <pavloromanenko25@gmail.com>
2021-03-12 15:52:27 +0200

    improved server-side test

--- tests/test_socket_modes_server.py
+++ tests/test_socket_modes_server.py
@@ -1,12 +1,45 @@
 import socket
+from time import time
+from asyncio import sleep
 
-with socket.socket() as sock:
-    sock.bind(('localhost', 8000))
-    sock.listen(1)
 
-    conn, addr = sock.accept()
-    conn.setblocking(False)
-    print('new conn from', addr)
+async def recvmsg(conn, bytescount, timeout):
+    source = b''
+    bytes_received = 0
+    used_timeout = 0
+    started_at = time()
 
-    while True:
-        print(conn.recv(1))
+    while (bytes_received < bytescount) or (used_timeout < timeout):
+        try:
+            if used_timeout:
+                used_timeout = 0
+
+            received = conn.recv(bytescount - bytes_received)
+            source += received
+            bytes_received += len(received)
+        except BlockingIOError:
+            print('temporary unavailable')
+            await sleep(.01)
+            used_timeout += time() - started_at
+
+    if used_timeout >= timeout:
+        return None
+
+    return source
+
+
+def main():
+    with socket.socket() as sock:
+        sock.bind(('localhost', 8000))
+        sock.listen(1)
+
+        conn, addr = sock.accept()
+        conn.setblocking(False)
+        print('new conn from', addr)
+
+        while True:
+            print(recvmsg(conn, 1, .7))
+
+
+if __name__ == '__main__':
+    main()
