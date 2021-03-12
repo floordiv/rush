b7a0f354e889abbfcfa18ac8d487b96c7b845a42
floordiv <pavloromanenko25@gmail.com>
2021-03-12 18:12:52 +0200

    added demo and client for aepollserverlib (client also available for any another things)

--- lib/aepollserverlib_demo.py
+++ lib/aepollserverlib_demo.py
@@ -1,13 +1,15 @@
 import aepollserver
+from lib.msgproto import recvmsg, sendmsg
 
 """
 this file is a good example (or even demo) of the work of the aepollserver lib
 """
 
-epoll_server = aepollserver.AEpollServer(('localhost', 8801))
+epoll_server = aepollserver.AEpollServer(('localhost', 8000))
 
 
 @epoll_server.handler(on_event=aepollserver.CONNECT)
+@aepollserver.handshake('server')
 async def handle_conn(_, server):
     conn, addr = server.accept()
     print('new conn from', addr)
@@ -17,8 +19,8 @@ async def handle_conn(_, server):
 
 @epoll_server.handler(aepollserver.RECEIVE)
 async def handle_recvmsg(_, conn: aepollserver.socket):
-    print(f'received data from {conn.getpeername()}: {conn.recv(10)}')
-    conn.send(b'hello!')
+    print(f'received data from {conn.getpeername()}: {await recvmsg(conn)}')
+    sendmsg(conn, b'hello!')
 
 
 @epoll_server.handler(aepollserver.DISCONNECT)
---
+++ lib/test_client.py
@@ -0,0 +1,21 @@
+import socket
+from asyncio import run
+
+from lib.msgproto import sendmsg, recvmsg
+from lib.epollserver import do_handshake
+
+
+async def main():
+    with socket.socket() as sock:
+        sock.connect(('localhost', 8000))
+        print('connected to localhost:8000')
+        await do_handshake(sock, 'server')
+        print('handshake succeeded')
+        sendmsg(sock, b'hello!')
+        print("sent: b'hello!'")
+        response = await recvmsg(sock)
+        print('received:', response)
+
+
+if __name__ == '__main__':
+    run(main())
